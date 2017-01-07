417f0748018b33473e66ab9378654c5be74992b3
Aiko Appeldorn <aiko@appeldorn.me>
2017-01-07 14:55:30 +0100

    Copy jail.local

--- setup-debian.sh
+++ setup-debian.sh
@@ -2,6 +2,7 @@
 apt-get install python3 python3-pyinotify python3-systemd -y
 python setup.py install
 touch /var/log/fail2ban.log
+cp config/jail.local /etc/fail2ban/
 
 cp files/debian-initd /etc/init.d/fail2ban
 update-rc.d fail2ban defaults
