b28ddd218d83b415a98d69764c11f7c2812e767d
Aiko Appeldorn <aiko@appeldorn.me>
2017-01-15 15:00:30 +0100

    Blocklist dialog

--- setup-debian.sh
+++ setup-debian.sh
@@ -9,7 +9,7 @@ email=$(\
           --inputbox "Gebe deine E-Mail ein mit der Fail2ban senden soll." 8 40 \
   3>&1 1>&2 2>&3 3>&- \
 )
-sed 's/^%EMAIL%$/$email /' /etc/fail2ban/jail.local &> /etc/fail2ban/jail.local
+sed -i 's/%EMAIL%/$email' /etc/fail2ban/jail.local
 
 dialog --title "Fail2ban Blocklist Einstellungen" --yesno "Blocklist.de konfigurieren?" 8 40
 response=$?
@@ -21,14 +21,15 @@ if [ $response = 0 ]
 						--inputbox "Gebe deinen API Key von Blocklist.de ein." 8 40 \
 				3>&1 1>&2 2>&3 3>&- \
 			)
-			sed 's/^%BLOCKLIST%$/$blocklist /' /etc/fail2ban/jail.local &> /etc/fail2ban/jail.local
-			sed 's/^%ACTION%$/action_blocklist_de /' /etc/fail2ban/jail.local &> /etc/fail2ban/jail.local
+			sed -i 's/%BLOCKLIST%/$blocklist' /etc/fail2ban/jail.local
+			sed -i 's/%ACTION%/action_blocklist_de' /etc/fail2ban/jail.local
         else
-        sed 's/^%ACTION%$/action_mwl /' /etc/fail2ban/jail.local &> /etc/fail2ban/jail.local
+		sed -i 's/%ACTION%/action_mwl' /etc/fail2ban/jail.local
 fi
 
 cp files/debian-initd /etc/init.d/fail2ban
 update-rc.d fail2ban defaults
 service fail2ban restart
+clear
 
 
