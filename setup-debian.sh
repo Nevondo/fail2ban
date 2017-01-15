ad50deb51f2ddf4e91dd89adf8bc685ba1c1f713
Aiko Appeldorn <aiko@appeldorn.me>
2017-01-15 14:53:20 +0100

    Blocklist dialog

--- setup-debian.sh
+++ setup-debian.sh
@@ -1,11 +1,34 @@
 #!/bin/sh
-apt-get install python3 python3-pyinotify python3-systemd -y
+apt-get install python3 python3-pyinotify python3-systemd dialog -y
 python setup.py install
 touch /var/log/fail2ban.log
 cp config/jail.local /etc/fail2ban/
+
+email=$(\
+   dialog --title "Fail2ban E-Mail Einstellungen" \
+          --inputbox "Gebe deine E-Mail ein mit der Fail2ban senden soll." 8 40 \
+  3>&1 1>&2 2>&3 3>&- \
+)
+sed 's/^%EMAIL%$/$(email) /' /etc/fail2ban/jail.local &> /etc/fail2ban/jail.local
+
+dialog --title "Fail2ban Blocklist Einstellungen" --yesno "Blocklist konfigurieren" 8 40
+response=$?
+
+if [ $response = 0 ]
+        then
+			blocklist=$(\
+				dialog 	--title "Fail2ban E-Blocklist Einstellungen" \
+						--inputbox "Gebe deinen API Key von Blocklist.de ein." 8 40 \
+				3>&1 1>&2 2>&3 3>&- \
+			)
+			sed 's/^%BLOCKLIST%$/$(blocklist) /' /etc/fail2ban/jail.local &> /etc/fail2ban/jail.local
+			sed 's/^%ACTION%$/action_blocklist_de /' /etc/fail2ban/jail.local &> /etc/fail2ban/jail.local
+        else
+        sed 's/^%ACTION%$/action_mwl /' /etc/fail2ban/jail.local &> /etc/fail2ban/jail.local
+fi
 
 cp files/debian-initd /etc/init.d/fail2ban
 update-rc.d fail2ban defaults
 service fail2ban stop
 
-echo "Configure jail.local and start Fail2ban with service fail2ban start!"
+
